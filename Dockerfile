afc7af4975e232d8113cb0456c1dbd60ce39e05d
aom19 <39848284+aom19@users.noreply.github.com>
2023-03-24 22:47:26 +0200

    set nginx and gunicorn

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,11 @@ FROM python:3.10
 
 RUN pip install --upgrade pip
 
-COPY ./requirements.txt .
+COPY ./req.txt .
 
-RUN pip install -r requirements.txt
+RUN pip install -r req.txt
 
-COPY ./django_project  /app
+COPY .    /app
 
 WORKDIR /app
 
--- cados_api/settings.py
+++ cados_api/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -110,6 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
